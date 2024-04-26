2587a5f30aaf6a4fb7149ff53e76e62db8e8c774
HendP <hendpermana12@gmail.com>
2024-04-26 16:19:02 +0700

    feat(platform): initialization platform

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import {k} from './kaboomCtx.ts';
+import {makeMap} from "./utils.ts";
+import {makePlayer} from "./entities.ts";
 
 async function gameSetup() {
     k.loadSprite("assets", './kirby-like.png', {
@@ -8,16 +10,21 @@ async function gameSetup() {
             kirbIdle: 0,
             kirbInhaling: 1,
             kirbFull: 2,
-            kirbInhaleEffect: { from: 3, to: 8, speed: 15, loop: true },
+            kirbInhaleEffect: {from: 3, to: 8, speed: 15, loop: true},
             shootingStar: 9,
-            flame: { from: 36, to: 37, speed: 4, loop: true },
+            flame: {from: 36, to: 37, speed: 4, loop: true},
             guyIdle: 28,
-            guyWalk: { from: 18, to: 19, speed: 4, loop: true },
-            bird: { from: 27, to: 28, speed: 4, loop: true },
+            guyWalk: {from: 18, to: 19, speed: 4, loop: true},
+            bird: {from: 27, to: 28, speed: 4, loop: true},
         }
     });
 
     k.loadSprite("level-1", "./level-1.png");
+
+    const {
+        map: level1Layout
+        , spawnPoints: level1SpawnPoints
+    } = await makeMap(k, "level-1");
 
     k.scene("level-1", () => {
         k.setGravity(2100);
@@ -26,6 +33,21 @@ async function gameSetup() {
             k.color(k.Color.fromHex("#f7d7db")),
             k.fixed()
         ]);
+
+        k.add(level1Layout);
+
+        const kirb = makePlayer(
+            k,
+            level1SpawnPoints.player[0].x,
+            level1SpawnPoints.player[0].y
+        );
+
+        k.add(kirb);
+        k.camScale(0.7, 0.7);
+        k.onUpdate(() => {
+            if (kirb.pos.x < level1Layout.pos.x + 432)
+                k.camPos(kirb.pos.x + 500, 800);
+        });
     });
 
     k.go("level-1");
